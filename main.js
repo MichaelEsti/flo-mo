564c76d5ed4a43982a7f60b9c5f300d998661436
MichaelEsti <michaelesti.css@gmail.com>
2021-12-29 19:09:49 -0500

    All changes

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+$(document).ready(function(){
+    $(".dropdown").click(function(){
+        $(this).find(".dropdown-menu").slideToggle("fast");
+    });
+});
+$(document).on("click", function(event){
+    var $trigger = $(".dropdown");
+    if($trigger !== event.target && !$trigger.has(event.target).length){
+        $(".dropdown-menu").slideUp("fast");
+    }
+});
+
+
+});
